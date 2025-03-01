e5a9e05651a38be39e292a1ccc3700f126b35cb9
@Dmytro.Reshetovskyi1 <dmytro.reshetovskyi@vista.com>
2025-03-01 18:28:54 +0200

    added fix

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { Roboto, Lusitana } from 'next/font/google';
+
+export const roboto = Roboto({
+    subsets: ["latin"],
+    weight: '400'
+});
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
